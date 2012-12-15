1ad486ae38f676114e9a2075b9e8bd7f5d26cdd4
reenboog <alex.gievsky@gmail.com>
2012-12-15 10:25:42 +0700

    Initial commit.

---
+++ old48Game/old48Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  old48Game
+//
+//  Created by Alex Gievsky on 15.12.12.
+//  Copyright spotGames 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
